f81d8f3aae24e2f9ce6eb082b816a91f9f568488
Pavel <p_romantsov@mail.ru>
2025-10-01 12:02:29 +0300

    Домашняя работа № 6 "ООО интерфейсы"

---
+++ IToDoService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleBotCommands;
+
+namespace ConsoleBotProgramm
+{
+    public interface IToDoService
+    {
+        IReadOnlyList<ToDoItem> GetAllByUserId(Guid userId);
+        IReadOnlyList<ToDoItem> GetActiveByUserId(Guid userId);
+        ToDoItem Add(ToDoUser user, string name);
+        void MarkCompleted(Guid id);
+        void Delete(Guid id);
+        int ParseAndValidateInt(string? str, int min, int max);
+        void ValidateString(string? str);
+    }
+
+}
