8622733e78c38e731f8e67fdac9b196bd5af0bf0
Pavel <p_romantsov@mail.ru>
2025-10-09 14:16:58 +0300

    Домашняя работа № 7 - добавление репозитория

--- ConsoleBot/Core/Exceptions/ClassExceptions.cs
+++ ConsoleBot/Core/Exceptions/ClassExceptions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleBotCommands
+namespace ConsoleBot.Core.Exceptions
 {
     public class TaskCountLimitException : Exception
     {
---
+++ ConsoleBot/Core/Services/UserService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleBot.Core.DataAccess;
+using ConsoleBot.Core.Entities;
+
+namespace ConsoleBot.Core.Services
+{
+    public class UserService : IUserService, IUserRepository
+    {
+        //private readonly IDictionary<long, ToDoUser> _users = new Dictionary<long, ToDoUser>();
+        private readonly IUserRepository _repository;
+
+        public ToDoUser RegisterUser(long telegramUserId, string telegramUserName)
+        {
+            var existingUser = _repository.GetUserByTelegramUserId(telegramUserId);
+            if (existingUser != null)
+            {
+                return existingUser;
+            }
+
+            var newUser = new ToDoUser(telegramUserId, telegramUserName);
+            _repository.Add(newUser);
+            return newUser;
+        }
+
+        public ToDoUser? GetUser(long telegramUserId)
+        {
+            return _repository.GetUserByTelegramUserId(telegramUserId);
+        }
+
+        public ToDoUser? GetUser(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ToDoUser? GetUserByTelegramUserId(long telegramUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(ToDoUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+    }
+}
